70b8b0752e24780e8f382def27b3dd786bca5172
asmatek <125524065+asmatek@users.noreply.github.com>
2025-01-19 12:17:48 -0600

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,11 +77,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
+
 
 
 # Password validation
